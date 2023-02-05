4776556b71f3513ce1b65fdef5a866ca2378aac3
Azat <azat.cdb@gmail.com>
2023-02-05 15:36:32 +0500

    get @Repository

--- src/main/java/azat/cdb/TGBotForShorts/service/FindNamesByJPG.java
+++ src/main/java/azat/cdb/TGBotForShorts/service/FindNamesByJPG.java
@@ -1,8 +1,10 @@
 package azat.cdb.TGBotForShorts.service;
 
+import org.springframework.stereotype.Repository;
+
 import java.io.IOException;
 import java.util.List;
-
+@Repository
 public interface FindNamesByJPG {
     public String getJPG(String url) throws IOException;
     public String getNamesByImg(String urlImg) throws IOException;
